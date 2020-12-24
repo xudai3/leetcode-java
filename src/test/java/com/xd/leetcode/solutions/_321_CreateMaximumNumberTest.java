2837e68afb12047a682890324e02ef5c7d95d032
xudai <xudai3@qq.com>
2020-12-24 21:15:51 +0800

    add 321.create-maximum-number

---
+++ src/test/java/com/xd/leetcode/solutions/_321_CreateMaximumNumberTest.java
@@ -0,0 +1,12 @@
+package com.xd.leetcode.solutions;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class _321_CreateMaximumNumberTest {
+
+    @Test
+    void maxNumber() {
+    }
+}
