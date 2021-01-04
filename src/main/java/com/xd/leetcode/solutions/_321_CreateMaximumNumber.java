d7cedfaba8e4d154ec9a75b6b5f918cdb708c552
xudai <xudai3@qq.com>
2021-01-04 19:14:52 +0800

    add 321.create-maximum-number

--- src/main/java/com/xd/leetcode/solutions/_321_CreateMaximumNumber.java
+++ src/main/java/com/xd/leetcode/solutions/_321_CreateMaximumNumber.java
@@ -1,5 +1,11 @@
 package com.xd.leetcode.solutions;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.stream.Collectors;
+
 /**
  * 给定长度分别为 m 和 n 的两个数组，其元素由 0-9 构成，表示两个自然数各位上的数字。现在从这两个数组中选出 k (k <= m + n) 个数字拼接成一个新的数，要求从同一个数组中取出的数字保持其在原数组中的相对顺序。
  *
@@ -38,6 +44,96 @@ package com.xd.leetcode.solutions;
 
 public class _321_CreateMaximumNumber {
     public int[] maxNumber(int[] nums1, int[] nums2, int k) {
-        return new int[]{};
+        List<List<Integer>> sequences = new ArrayList<>();
+        int len1 = nums1.length;
+        int len2 = nums2.length;
+        for (int i = 0; i <= len1; i++) {
+            int j = k - i;
+            if (j > len2 || i > k) {
+                continue;
+            }
+            int[] subNums1 = getMaxSubSequence(nums1, i);
+            int[] subNums2 = getMaxSubSequence(nums2, j);
+            int[] subMaxNums = mergeSubSequence(subNums1, subNums2);
+            sequences.add(Arrays.stream(subMaxNums).boxed().collect(Collectors.toList()));
+        }
+        return getMaxSequence(sequences);
+    }
+    public int[] getMaxSubSequence(int[] nums, int n) {
+        LinkedList<Integer> stack = new LinkedList<>();
+        int len = nums.length;
+        int removed = 0;
+        for (int i = 0; i < len; i++) {
+            int num = nums[i];
+            while (!stack.isEmpty() && stack.getLast() < num && removed < len-n) {
+                stack.removeLast();
+                removed++;
+            }
+            stack.add(num);
+        }
+        int[] maxSequence = new int[n];
+        for (int i = 0; i < n; i++) {
+            maxSequence[i] = stack.removeFirst();
+        }
+        return maxSequence;
+    }
+    public int[] mergeSubSequence(int[] nums1, int[] nums2) {
+        int i = 0;
+        int j = 0;
+        int len1 = nums1.length;
+        int len2 = nums2.length;
+        int[] newArr = new int[len1 + len2];
+        for (int idx = 0; idx < len1+len2; idx++) {
+            if (compareArr(nums1, i, nums2, j) > 0) {
+                newArr[idx] = nums1[i++];
+            } else {
+                newArr[idx] = nums2[j++];
+            }
+        }
+        return newArr;
+    }
+    public int compareArr(int[] nums1, int idx1, int[] nums2, int idx2) {
+        int len1 = nums1.length;
+        int len2 = nums2.length;
+        while (idx1 < len1 && idx2 < len2) {
+            int diff = nums1[idx1] - nums2[idx2];
+            if (diff != 0) {
+                return diff;
+            }
+            idx1++;
+            idx2++;
+        }
+        return (len1 - idx1) - (len2 - idx2);
+    }
+    public int compareStr(String str1, String str2) {
+        int len1 = str1.length();
+        int len2 = str2.length();
+        if (len1 != len2) {
+            return len1 - len2;
+        }
+        for (int i = 0; i < len1; i++) {
+            int diff = str1.charAt(i) - str2.charAt(i);
+            if (diff != 0) {
+                return diff;
+            }
+        }
+        return 0;
+    }
+    public int[] getMaxSequence(List<List<Integer>> sequences) {
+        String max = "";
+        int idx = 0;
+        for (int i = 0; i < sequences.size(); i++) {
+            String numStr = "";
+            List<Integer> sequence = sequences.get(i);
+            int len = sequence.size();
+            for (int j = 0; j < len; j++) {
+                numStr += sequence.get(j);
+            }
+            if (compareStr(numStr, max) > 0) {
+                max = numStr;
+                idx = i;
+            }
+        }
+        return sequences.get(idx).stream().mapToInt(i -> i).toArray();
     }
 }
--- src/test/java/com/xd/leetcode/solutions/_321_CreateMaximumNumberTest.java
+++ src/test/java/com/xd/leetcode/solutions/_321_CreateMaximumNumberTest.java
@@ -1,12 +1,48 @@
 package com.xd.leetcode.solutions;
 
+import com.xd.leetcode.utils.Utils;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class _321_CreateMaximumNumberTest {
+    _321_CreateMaximumNumber c;
+    Utils u;
+    int[][] nums1;
+    int[][] nums2;
+    int[] k;
+    String[] expect;
+    _321_CreateMaximumNumberTest() {
+        c = new _321_CreateMaximumNumber();
+        u = new Utils();
+        nums1 = new int[][]{{3,4,6,5}, {6,7}, {3,9}, {9,1,2,5,8,3}, {2,5,6,4,4,0}, {8,5,9,5,1,6,9}};
+        nums2 = new int[][]{{9,1,2,5,8,3}, {6,0,4}, {8,9}, {3,4,6,5}, {7,3,8,0,6,5,7,6,2}, {2,6,4,3,8,4,1,0,7,2,9,2,8}};
+        k = new int[]{5, 5, 3, 5, 15, 20};
+        expect = new String[]{"98653", "67604", "989", "98653", "738256440657620", "85952643841691072928"};
+    }
 
     @Test
     void maxNumber() {
+        for (int i = 0; i < nums1.length; i++) {
+            int[] output = c.maxNumber(nums1[i], nums2[i], k[i]);
+            assertEquals(u.intArrayToString(output), expect[i]);
+        }
+    }
+
+    @Test
+    void getMaxSequence() {
+        int[] input = new int[]{5, 4, 8, 3};
+        int[] output = c.getMaxSubSequence(input, 3);
+        System.out.println(Arrays.toString(output));
+    }
+
+    @Test
+    void mergeSubSequence() {
+        int[] nums1 = new int[]{6};
+        int[] nums2 = new int[]{9, 5, 8, 3};
+        int[] output = c.mergeSubSequence(nums1, nums2);
+        System.out.println(Arrays.toString(output));
     }
 }
