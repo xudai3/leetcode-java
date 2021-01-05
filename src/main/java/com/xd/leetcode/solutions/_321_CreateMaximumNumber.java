3950a98cd5bd5890b7f798bbe5b627d16da2ed86
xudai <xudai3@qq.com>
2021-01-05 18:01:05 +0800

    优化 321.create-maximum-number的执行效率

--- src/main/java/com/xd/leetcode/solutions/_321_CreateMaximumNumber.java
+++ src/main/java/com/xd/leetcode/solutions/_321_CreateMaximumNumber.java
@@ -1,11 +1,5 @@
 package com.xd.leetcode.solutions;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.stream.Collectors;
-
 /**
  * 给定长度分别为 m 和 n 的两个数组，其元素由 0-9 构成，表示两个自然数各位上的数字。现在从这两个数组中选出 k (k <= m + n) 个数字拼接成一个新的数，要求从同一个数组中取出的数字保持其在原数组中的相对顺序。
  *
@@ -52,49 +46,63 @@ public class _321_CreateMaximumNumber {
      * @return
      */
     public int[] maxNumber(int[] nums1, int[] nums2, int k) {
-        List<List<Integer>> sequences = new ArrayList<>();
         int len1 = nums1.length;
         int len2 = nums2.length;
-        // 遍历i+j=k
-        for (int i = 0; i <= len1; i++) {
-            int j = k - i;
-            if (j > len2 || i > k) {
-                continue;
+        int[] maxSequence = new int[k];
+        // 如果len2=3而k=5，则第一个数组至少要选2个数出来
+        int start = Math.max(0, k-len2);
+        // 如果len1=5而k=3，则第一个数组最多也只能选3个数
+        int end = Math.min(k, len1);
+        for (int i = start; i <= end; i++) {
+            int[] subNums1 = maxSubsequence(nums1, i);
+            int[] subNums2 = maxSubsequence(nums2, k-i);
+            int[] subMaxNums = merge(subNums1, subNums2);
+            // 用maxSequence存储每次比较的最大值，之前是用二维数组存储再找最大，节约了一些时间
+            if (compare(subMaxNums, 0, maxSequence, 0) > 0) {
+                System.arraycopy(subMaxNums, 0, maxSequence, 0, k);
             }
-            int[] subNums1 = getMaxSubSequence(nums1, i);
-            int[] subNums2 = getMaxSubSequence(nums2, j);
-            int[] subMaxNums = mergeSubSequence(subNums1, subNums2);
-            sequences.add(Arrays.stream(subMaxNums).boxed().collect(Collectors.toList()));
-        }
-        return getMaxSequence(sequences);
-    }
-    public int[] getMaxSubSequence(int[] nums, int n) {
-        LinkedList<Integer> stack = new LinkedList<>();
-        int len = nums.length;
-        int removed = 0;
-        for (int i = 0; i < len; i++) {
-            int num = nums[i];
-            while (!stack.isEmpty() && stack.getLast() < num && removed < len-n) {
-                stack.removeLast();
-                removed++;
-            }
-            stack.add(num);
-        }
-        int[] maxSequence = new int[n];
-        for (int i = 0; i < n; i++) {
-            maxSequence[i] = stack.removeFirst();
         }
         return maxSequence;
     }
-    public int[] mergeSubSequence(int[] nums1, int[] nums2) {
-        int i = 0;
-        int j = 0;
+    // 直接使用数组模拟栈而不用LinkedList来模拟栈可以直接返回结果，不用从LinkedList里取出来放到数组里，节约了一些时间
+    public int[] maxSubsequence(int[] nums, int k) {
+        int length = nums.length;
+        int[] stack = new int[k];
+        // 栈顶
+        int top = -1;
+        // 栈剩余空间
+        int remain = length - k;
+        for (int i = 0; i < length; i++) {
+            int num = nums[i];
+            while (top >= 0 && stack[top] < num && remain > 0) {
+                top--;
+                remain--;
+            }
+            if (top < k - 1) {
+                stack[++top] = num;
+            } else {
+                // top = k - 1的时候就已经是没有剩余空间了，remain--跳出循环
+                remain--;
+            }
+        }
+        return stack;
+    }
+    public int[] merge(int[] nums1, int[] nums2) {
         int len1 = nums1.length;
         int len2 = nums2.length;
-        int[] newArr = new int[len1 + len2];
-        for (int idx = 0; idx < len1+len2; idx++) {
+        if (len1 == 0) {
+            return nums2;
+        }
+        if (len2 == 0) {
+            return nums1;
+        }
+        int mergeLen = len1 + len2;
+        int[] newArr = new int[mergeLen];
+        int i = 0;
+        int j = 0;
+        for (int idx = 0; idx < mergeLen; idx++) {
             // 合并的时候每次取数组前面最大的那个数字，如果相等的话则需要继续向后比较
-            if (compareArr(nums1, i, nums2, j) > 0) {
+            if (compare(nums1, i, nums2, j) > 0) {
                 newArr[idx] = nums1[i++];
             } else {
                 newArr[idx] = nums2[j++];
@@ -102,9 +110,8 @@ public class _321_CreateMaximumNumber {
         }
         return newArr;
     }
-    public int compareArr(int[] nums1, int idx1, int[] nums2, int idx2) {
-        int len1 = nums1.length;
-        int len2 = nums2.length;
+    public int compare(int[] nums1, int idx1, int[] nums2, int idx2) {
+        int len1 = nums1.length, len2 = nums2.length;
         while (idx1 < len1 && idx2 < len2) {
             int diff = nums1[idx1] - nums2[idx2];
             if (diff != 0) {
@@ -114,37 +121,5 @@ public class _321_CreateMaximumNumber {
             idx2++;
         }
         return (len1 - idx1) - (len2 - idx2);
-    }
-    public int compareStr(String str1, String str2) {
-        int len1 = str1.length();
-        int len2 = str2.length();
-        if (len1 != len2) {
-            return len1 - len2;
-        }
-        for (int i = 0; i < len1; i++) {
-            int diff = str1.charAt(i) - str2.charAt(i);
-            if (diff != 0) {
-                return diff;
-            }
-        }
-        return 0;
-    }
-    public int[] getMaxSequence(List<List<Integer>> sequences) {
-        String max = "";
-        int idx = 0;
-        for (int i = 0; i < sequences.size(); i++) {
-            String numStr = "";
-            List<Integer> sequence = sequences.get(i);
-            int len = sequence.size();
-            for (int j = 0; j < len; j++) {
-                numStr += sequence.get(j);
-            }
-            // 有的case已经超过了long的范围，所以需要自己进行比较，这里使用转换成字符串再比较的方法
-            if (compareStr(numStr, max) > 0) {
-                max = numStr;
-                idx = i;
-            }
-        }
-        return sequences.get(idx).stream().mapToInt(i -> i).toArray();
     }
 }
