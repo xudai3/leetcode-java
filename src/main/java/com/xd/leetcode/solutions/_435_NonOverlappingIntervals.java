6ceea30d4937aad20411c9926f669ab5a9ecc139
xudai <xudai3@qq.com>
2021-01-06 21:00:23 +0800

    435.non-overlapping-intervals

--- src/main/java/com/xd/leetcode/solutions/_435_NonOverlappingIntervals.java
+++ src/main/java/com/xd/leetcode/solutions/_435_NonOverlappingIntervals.java
@@ -1,5 +1,8 @@
 package com.xd.leetcode.solutions;
 
+import java.util.Arrays;
+import java.util.Comparator;
+
 /**
  *给定一个区间的集合，找到需要移除区间的最小数量，使剩余区间互不重叠。
  *
@@ -31,7 +34,66 @@ package com.xd.leetcode.solutions;
  *
  */
 public class _435_NonOverlappingIntervals {
+    /**
+     * 先将所有的区间按起点从小到大排序
+     * 先取第一段终点和第二段起点比较是否重叠，如果有重叠继续以更小的终点为基准比较下去，直到找到下一个没有重叠的区间
+     * 再以这个区间作为基准继续往下进行比较
+     * @param intervals
+     * @return
+     */
+    public int eraseOverlapIntervals2(int[][] intervals) {
+        int n = intervals.length;
+        if (n <= 1) {
+            return 0;
+        }
+        // 速度 写法1 > 写法2 >= 写法3
+        // 排序写法1
+//        Arrays.sort(intervals, new Comparator<int[]>() {
+//            @Override
+//            public int compare(int[] o1, int[] o2) {
+//                return o1[0] - o2[0];
+//            }
+//        });
+        // 排序写法2
+//        Arrays.sort(intervals, (a, b) -> a[0] - b[0]);
+        // 排序写法3
+        Arrays.sort(intervals, Comparator.comparingInt(o -> o[0]));
+        int cnt = 0;
+        int prevEnd = intervals[0][1];
+        for (int i = 1; i < n; i++) {
+            if (intervals[i][0] < prevEnd) {
+                prevEnd = Math.min(prevEnd, intervals[i][1]);
+                cnt++;
+            } else {
+                prevEnd = intervals[i][1];
+            }
+        }
+        return cnt;
+    }
+    /**
+     * 先将所有的区间按终点从小到大排序
+     * 先取第一段终点和第二段起点比较是否重叠
+     * 如果有重叠继续以第一段终点(因为排序后已经是最小，更短的区间可以保留后面更多的不重叠区间)为基准比较下去
+     * 直到找到下一个没有重叠的区间
+     * 再以这个区间作为基准继续往下进行比较
+     * @param intervals
+     * @return
+     */
     public int eraseOverlapIntervals(int[][] intervals) {
-
+        int n = intervals.length;
+        if (n <= 1) {
+            return 0;
+        }
+        Arrays.sort(intervals, Comparator.comparingInt(o -> o[1]));
+        int prevEnd = intervals[0][1];
+        int cnt = 0;
+        for (int i = 1; i < n; i++) {
+            if (intervals[i][0] < prevEnd) {
+                cnt++;
+            } else {
+                prevEnd = intervals[i][1];
+            }
+        }
+        return cnt;
     }
 }
