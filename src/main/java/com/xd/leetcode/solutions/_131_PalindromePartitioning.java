db81e6f3f2e7f6f5f75754285b1add9b1d56a99c
xudai <xudai3@qq.com>
2021-01-22 21:01:00 +0800

    add 131.palindrome-partitioning

---
+++ src/main/java/com/xd/leetcode/solutions/_131_PalindromePartitioning.java
@@ -0,0 +1,31 @@
+package com.xd.leetcode.solutions;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 给定一个字符串 s，将 s 分割成一些子串，使每个子串都是回文串。
+ *
+ * 返回 s 所有可能的分割方案。
+ *
+ * 示例:
+ *
+ * 输入: "aab"
+ * 输出:
+ * [
+ *   ["aa","b"],
+ *   ["a","a","b"]
+ * ]
+ *
+ */
+public class _131_PalindromePartitioning {
+    /**
+     * abba
+     * @param s
+     * @return
+     */
+    public List<List<String>> partition(String s) {
+        List<List<String>> res = new ArrayList<>();
+
+    }
+}
